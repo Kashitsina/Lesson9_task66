49bb28e0960a611222c96400acfd846ca6ec6b19
Anna <107642838+Kashitsina@users.noreply.github.com>
2022-07-31 20:09:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿void Zadacha66()
+{
+ //Задайте значения m и n. Напишите программу, 
+ //которая найдёт сумму натуральных элементов в промежутке от m до n
+
+ int m = InputNumbers("Введите m: ");
+ int n = InputNumbers("Введите n: ");
+ int temp = m;
+
+ if (m > n) 
+ {
+   m = n; 
+   n = temp;
+ }
+
+ PrintSumm(m, n, temp=0);
+
+ void PrintSumm(int m, int n, int summ)
+ {
+   summ = summ + n;
+   if (n <= m)
+   {
+     Console.Write($"Сумма элементов= {summ} ");
+     return;
+   }
+   PrintSumm(m, n - 1, summ);
+ }
+
+ int InputNumbers(string input) 
+ {
+   Console.Write(input);
+   int output = Convert.ToInt32(Console.ReadLine());
+   return output;
+ }
+}
+Zadacha66();
